07fa1f7829dc3782503284b30684095f2e142947
swayamrajora <127737419+swayamrajora@users.noreply.github.com>
2025-05-14 08:11:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+//your JS code here. If required.
+function mapLetters(word) {
+  const result = {};
+
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
+
+    if (!result[letter]) {
+      result[letter] = [];
+    }
+
+    result[letter].push(i);
+  }
+
+  return result;
+}
+//map letter in strings
